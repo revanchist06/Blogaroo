4609320f01fcb7f49493c54c7efa297337d3eba2
revanchist06 <jaideep0610s@gmail.com>
2024-02-20 10:31:13 +0530

    First Post

--- blogaroo/blog/views.py
+++ blogaroo/blog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
-
+from .models import BPost
 # Create your views here.
+
+def post_list(request):
+    posts = BPost.objects.all()
+    print("#"*100)
+    print(posts)
+    print(dir(posts))
+    print("#"*100)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+def post_detail(request, id):
+    post = BPost.objects.get(id=id)
+    return render(request, 'blog/post/detail.html',{'post':post})
+
+
+"""
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                 'blog/post/list.html',
+                 {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+
+"""
